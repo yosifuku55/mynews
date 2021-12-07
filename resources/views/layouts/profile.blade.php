457ccf2f46f9364a669d4cc065ea718ac2a10c65
midori <m.fukuno0519@gmail.com>
2021-12-07 13:48:33 +0000

    コメント

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -21,10 +21,10 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
-        {{-- 次の1行を削除 --}}
+        {{-- Laravel標準で用意されているCSSを読み込みます --}}
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+        {{-- この章の後半で作成するCSSを読み込みます --}}
         <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">
-        {{-- 次の1行を追記 --}}
-        <link href="{{ secure_asset('css/profile.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="app">
@@ -46,6 +46,34 @@
 
                         <!-- Right Side Of Navbar -->
                         <ul class="navbar-nav ml-auto">
+                            
+                                {{-- 以下を追記 --}}
+                        <!-- Authentication Links -->
+                        {{-- ログインしていなかったらログイン画面へのリンクを表示 --}}
+                        @guest
+                            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                            
+                            {{-- ログインしていたらユーザー名とログアウトボタンを表示 --}}
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+                                
+                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+                                    
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                            @endguest
+                            {{-- 以上までを追記 --}}
                         </ul>
                     </div>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,14 @@ Route::get('/', function () {
 Route::get('xxx', 'Action\AAAController@bbb');
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add'); 
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
+    Route::get('XXX', 'AAAController@bbb');
+    Route::get('admin/profile/edit', 'Admin\ProfileController@add')->middleware('auth');
+    Auth::routes();
 
 
-
+Route::get('/home', 'HomeController@index')->name('home');
 
