a471942dd78a1afab710b8410f73d51378f5f602
midori <m.fukuno0519@gmail.com>
2022-01-19 12:52:22 +0000

    コメント

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 // 以下を追記することでNews Modelが扱えるようになる
-use app\profile;
+use App\Profile;
 
 class ProfileController extends Controller
 {
@@ -16,14 +16,29 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
     
-    public function create()  
+    public function create(Request $request)  
     {
+        // Varidationを行う
+        // 今回はプローフィールの値チェック(newsじゃない)
+         $this->validate($request, Profile::$rules);
+        
+        // 今回はプローフィール登録(newsじゃない)
+        $Myprofile = new Profile;
+        
+        $form = $request->all();
+       
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        
+         // データベースに保存する
+        $Myprofile->fill($form);
+        $Myprofile->save();
         return redirect('admin/profile/create');
     }
     
     public function edit()
     {
-        return view('admin.profile.edit');
+        return redirect('admin/profile/edit');
     }
     
     public function update()
@@ -33,22 +48,6 @@ class ProfileController extends Controller
 
     public function Controllers(Request $request)
     {
-        
-        // Varidationを行う
-        // 今回はプローフィールの値チェック(newsじゃない)
-         $this->validate($request, profile::$rules);
-        
-        // 今回はプローフィール登録(newsじゃない)
-        $Myprofile = Myprofile;
-        
-        $form = $request->all();
-       
-        // フォームから送信されてきた_tokenを削除する
-        unset($form['_token']);
-        
-         // データベースに保存する
-        $myprofile->fill($form);
-        $myprofile->save();
+
     }
-    
 }
