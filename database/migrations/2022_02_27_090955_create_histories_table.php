fb21bfc6f34ecd2418b4536e59352960b7eeaa30
midori <m.fukuno0519@gmail.com>
2022-03-20 02:34:51 +0000

    コメント

--- database/migrations/2022_02_27_090955_create_histories_table.php
+++ database/migrations/2022_02_27_090955_create_histories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateHistoriesTable extends Migration
+class CreateProfileHistoriesTable extends Migration
 {
     /**
      * Run the migrations.
