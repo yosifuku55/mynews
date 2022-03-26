b1e1b75d094e3b9ee6255204649ce07ca9dd1f39
midori <m.fukuno0519@gmail.com>
2022-03-26 02:23:05 +0000

    コメント

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -12,6 +12,7 @@ use App\History;
 
 // 以下を追記
 use Carbon\Carbon;
+use Storage;
 
 class NewsController extends Controller
 {
@@ -33,8 +34,8 @@ class NewsController extends Controller
         
          // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
       if (isset($form['image'])) {
-        $path = $request->file('image')->store('public/image');
-        $news->image_path = basename($path);
+        $path = Storage::disk('s3')->putFile('/',$form['image'],'public'); 
+        $news->image_path = Storage::disk('s3')->url($path);    
       } else {
           $news->image_path = null;
       }
@@ -89,8 +90,8 @@ class NewsController extends Controller
       if ($request->remove == 'true') {
           $news_form['image_path'] = null;
       } elseif ($request->file('image')) {
-          $path = $request->file('image')->store('public/image');
-          $news_form['image_path'] = basename($path);
+         $path = Storage::disk('s3')->putFile('/',$news_form['image'],'public');
+         $news->image_path = Storage::disk('s3')->url($path);
       } else {
           $news_form['image_path'] = $news->image_path;
       }
